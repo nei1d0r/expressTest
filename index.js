3f022153d2731f353c42599f0d188a106eac1d3d
Neil Davis <neil.davis@easyfundraising.org.uk>
2019-12-16 12:44:52 +0000

    added some routes for testing

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ const PORT = process.env.PORT || 5000
 
 // app.express gives us access to lots of methods, such as listen
 // we can listen to a specific port
+// pass in a callback to print out when connected
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+// create main route
+app.get('/' , (req, res) => {
+    res.send('hello world')
+})
+
+// create about route
+app.get('/about' , (req, res) => {
+    res.send('This is About')
+})
